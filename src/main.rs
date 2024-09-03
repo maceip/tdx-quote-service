5ec5ab9627d53c2d7dca1b13dbb9da5092b133df
Your Name <you@example.com>
2024-09-03 05:32:05 +0000

    collateral fix

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ use tdx_attest_rs;
 use tokio::net::TcpListener;
 use tokio_rustls::TlsAcceptor;
 pub const REPORT_DATA_SIZE: usize = 64;
+use std::mem;
 
 fn main() {
     if let Err(e) = run_server() {
@@ -89,15 +90,6 @@ async fn run_server() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
 async fn echo(req: Request<Incoming>) -> Result<Response<Full<Bytes>>, hyper::Error> {
     let mut response = Response::new(Full::default());
-    let mut collateral_expiration_status = 1u32;
-    let mut quote_verification_result = sgx_ql_qv_result_t::SGX_QL_QV_RESULT_UNSPECIFIED;
-
-    let mut supp_data: sgx_ql_qv_supplemental_t = Default::default();
-    let mut supp_data_desc = tee_supp_data_descriptor_t {
-        major_version: 0,
-        data_size: 0,
-        p_data: &mut supp_data as *mut sgx_ql_qv_supplemental_t as *mut u8,
-    };
 
     match (req.method(), req.uri().path()) {
         // Help route.
@@ -153,35 +145,135 @@ async fn echo(req: Request<Incoming>) -> Result<Response<Full<Bytes>>, hyper::Er
                 .await?
                 .aggregate();
 
-            // Ensure that the body is the correct size for tdx_report_data_t
-            if whole_body.remaining() != 8000 {
-                *response.status_mut() = StatusCode::BAD_REQUEST;
-            }
 
             // Convert the body into tdx_report_data_t
-            let mut user_quote = [0u8; 8000];
+            let mut user_quote = [0u8; 2722];
             whole_body.copy_to_slice(&mut user_quote);
+            let mut collateral_expiration_status = 1u32;
+            let mut quote_verification_result = sgx_ql_qv_result_t::SGX_QL_QV_RESULT_UNSPECIFIED;
+        
+            let mut supp_data: sgx_ql_qv_supplemental_t = Default::default();
+            let mut supp_data_desc = tee_supp_data_descriptor_t {
+                major_version: 0,
+                data_size: 0,
+                p_data: &mut supp_data as *mut sgx_ql_qv_supplemental_t as *mut u8,
+            };
+        
 
+            match tee_get_supplemental_data_version_and_size(&user_quote) {
+                Ok((supp_ver, supp_size)) => {
+                    if supp_size == mem::size_of::<sgx_ql_qv_supplemental_t>() as u32 {
+                        println!("\tInfo: tee_get_quote_supplemental_data_version_and_size successfully returned.");
+                        println!("\tInfo: latest supplemental data major version: {}, minor version: {}, size: {}",
+                            u16::from_be_bytes(supp_ver.to_be_bytes()[..2].try_into().unwrap()),
+                            u16::from_be_bytes(supp_ver.to_be_bytes()[2..].try_into().unwrap()),
+                            supp_size,
+                        );
+                        supp_data_desc.data_size = supp_size;
+                    } else {
+                        println!("\tWarning: Quote supplemental data size is different between DCAP QVL and QvE, please make sure you installed DCAP QVL and QvE from same release.")
+                    }
+                }
+                Err(e) => println!(
+                    "\tError: tee_get_quote_supplemental_data_size failed: {:#04x}",
+                    e as u32
+                ),
+            }
+    
+            // get collateral
             let collateral = tee_qv_get_collateral(&user_quote);
+            match collateral {
+                Ok(ref c) => println!("\tInfo: tee_qv_get_collateral successfully returned."),
+                Err(e) => println!("\tError: tee_qv_get_collateral failed: {:#04x}", e as u32),
+            };
+    
+            // set current time. This is only for sample purposes, in production mode a trusted time should be used.
+            //
             let current_time = SystemTime::now()
                 .duration_since(SystemTime::UNIX_EPOCH)
                 .unwrap_or(Duration::ZERO)
                 .as_secs() as i64;
-
+    
+            let p_supplemental_data = match supp_data_desc.data_size {
+                0 => None,
+                _ => Some(&mut supp_data_desc),
+            };
+    
+            // call DCAP quote verify library for quote verification
+            // here you can choose 'trusted' or 'untrusted' quote verification by specifying parameter '&qve_report_info'
+            // if '&qve_report_info' is NOT NULL, this API will call Intel QvE to verify quote
+            // if '&qve_report_info' is NULL, this API will call 'untrusted quote verify lib' to verify quote, this mode doesn't rely on SGX capable system, but the results can not be cryptographically authenticated
             match tee_verify_quote(
                 &user_quote,
                 collateral.ok().as_ref(),
                 current_time,
                 None,
-                None,
+                p_supplemental_data,
             ) {
                 Ok((colla_exp_stat, qv_result)) => {
                     collateral_expiration_status = colla_exp_stat;
                     quote_verification_result = qv_result;
                     println!("\tInfo: App: tee_verify_quote successfully returned.");
-                    *response.body_mut() = Full::from(colla_exp_stat.to_string());
                 }
                 Err(e) => println!("\tError: App: tee_verify_quote failed: {:#04x}", e as u32),
+            }
+
+            match quote_verification_result {
+                sgx_ql_qv_result_t::SGX_QL_QV_RESULT_OK => {
+                    // check verification collateral expiration status
+                    // this value should be considered in your own attestation/verification policy
+                    //
+                    if collateral_expiration_status == 0 {
+                        println!("\tInfo: App: Verification completed successfully.");
+                    } else {
+                        println!("\tWarning: App: Verification completed, but collateral is out of date based on 'expiration_check_date' you provided.");
+                    }
+                }
+                sgx_ql_qv_result_t::SGX_QL_QV_RESULT_CONFIG_NEEDED
+                | sgx_ql_qv_result_t::SGX_QL_QV_RESULT_OUT_OF_DATE
+                | sgx_ql_qv_result_t::SGX_QL_QV_RESULT_OUT_OF_DATE_CONFIG_NEEDED
+                | sgx_ql_qv_result_t::SGX_QL_QV_RESULT_SW_HARDENING_NEEDED
+                | sgx_ql_qv_result_t::SGX_QL_QV_RESULT_CONFIG_AND_SW_HARDENING_NEEDED => {
+                    println!(
+                        "\tWarning: App: Verification completed with Non-terminal result: {:x}",
+                        quote_verification_result as u32
+                    );
+                }
+                sgx_ql_qv_result_t::SGX_QL_QV_RESULT_INVALID_SIGNATURE
+                | sgx_ql_qv_result_t::SGX_QL_QV_RESULT_REVOKED
+                | sgx_ql_qv_result_t::SGX_QL_QV_RESULT_UNSPECIFIED
+                | _ => {
+                    println!(
+                        "\tError: App: Verification completed with Terminal result: {:x}",
+                        quote_verification_result as u32
+                    );
+                }
+            }
+        
+            // check supplemental data if necessary
+            //
+            if supp_data_desc.data_size > 0 {
+                // you can check supplemental data based on your own attestation/verification policy
+                // here we only print supplemental data version for demo usage
+                //
+                let version_s = unsafe { supp_data.__bindgen_anon_1.__bindgen_anon_1 };
+                println!(
+                    "\tInfo: Supplemental data Major Version: {}",
+                    version_s.major_version
+                );
+                println!(
+                    "\tInfo: Supplemental data Minor Version: {}",
+                    version_s.minor_version
+                );
+        
+                // print SA list if exist, SA list is supported from version 3.1
+                //
+                if unsafe { supp_data.__bindgen_anon_1.version } > 3 {
+                    let sa_list = unsafe { std::ffi::CStr::from_ptr(supp_data.sa_list.as_ptr()) };
+                    if sa_list.to_bytes().len() > 0 {
+                        println!("\tInfo: Advisory ID: {}", sa_list.to_str().unwrap());
+                    }
+                }
             }
         }
         // Catch-all 404.
